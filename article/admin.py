bada7a3fb1a3f37c963abc8be54da7fd15f2f3ca
nblity007 <liux3113@gmail.com>
2018-04-18 14:26:52 +0800

    后台添加Article模块

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+#from article.models import Article
+from article.models import Article  # @UnresolvedImport
 
 # Register your models here.
+
+admin.site.register(Article)
